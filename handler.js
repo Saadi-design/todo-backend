6430e1449827d283935da528638c7ce8bd7dde46
Saadiya Moid <saadiyamoid@gmail.com>
2019-11-26 13:53:01 +0000

    new changes

--- handler.js
+++ handler.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const serverlessHttp = require("serverless-http");
 const cors = require("cors");
+const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 
 const app = express();
+
 app.use(cors());
 
-app.get("/tasks", function (request, response) {
-  const username = request.query.username;
-    
-  const sometasks = [
+app.use(bodyParser.json());
+
+app.get("/tasks", function(request, response){
+  response.status(200).send({
+    tasks: [
       {
           id : 001,
           taskDescription: "order inhalers",
@@ -52,26 +54,26 @@ app.get("/tasks", function (request, response) {
           creationDate: "2019-10-30",
           user: 1,
       }
-
+    
   ]
-})
-  response.status(200).send("Getting all the tasks! Yay!");
-
-
-app.post("/tasks", function (request, response) {
-
-  const task = request.body;
-
-  response.status(201).send("Successfully created task: " + task.text);
 });
-app.delete("/tasks/:taskId", function (request, response) {
-  const taskId = request.params.taskId;
-  response.status(200).send("Deleted task with id " + taskId);
-});
-app.put("/tasks/:taskId", function (request, response) {
-  const taskId = request.params.taskId;
-  const updatedTask = request.body;
-  response.status(200).send("Updated task with id " + taskId);
 });
 
-module.exports.tasks = serverlessHttp(app);
+app.delete("/tasks/:taskId", function(request, response){
+const id = request.params.taskId;
+response.status(200).send("Received a request to delete task ID: "+id);
+});
+
+app.post("/tasks", function(request, response){
+//Create a new task
+const task = request.body;
+response.status(201).send("Created a new task with text: "+task.text);
+});
+
+app.put("/tasks/:taskId",function (request, response){
+// Update task
+const id = request.params.taskId;
+const task = request.body;
+response.status(200).send("Received request to update task: " + task.text);
+});
+module.exports.tasks = serverless(app);
