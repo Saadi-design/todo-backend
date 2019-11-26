55f22a556766117b52d6cbd136d2c18f2fa1d798
Saadiya Moid <saadiyamoid@gmail.com>
2019-11-26 14:37:09 +0000

    new changes

--- handler.js
+++ handler.js
@@ -1,79 +1,88 @@
 const express = require("express");
+const serverlessHttp = require("serverless-http");
 const cors = require("cors");
-const serverless = require("serverless-http");
-const bodyParser = require("body-parser");
+const bodyparser= require("body-parser");
 
 const app = express();
 
+// allows Express to parse JSON ddata that is sent on the body of any requests
+app.use(bodyparser.json())
 app.use(cors());
 
-app.use(bodyParser.json());
 
-app.get("/tasks", function(request, response){
+app.get("/tasks", function (request, response) {
+  // Do the logic for getting all the tasks from the DB
   response.status(200).send({
     tasks: [
-      {
-          id : 001,
-          taskDescription: "order inhalers",
-          completed: false,
-          creationDate: "2019-11-19",
-          user: 2,
-      },
-      {
-          id : 002,
-          taskDescription: "complete shopping for trip",
-          completed: true,
-          creationDate: "2019-11-15",
-          user: 1,
-      },
-      {
-          id : 003,
-          taskDescription: "book flight",
-          completed: false,
-          creationDate: "2019-11-18",
-          user: 1,
-      },
-      {
-          id : 004,
-          taskDescription: "complete CV",
-          completed: true,
-          creationDate: 2019-11-05,
-          user: 1,
-      }
-      {
-          id : 005,
-          taskDescription: "pay credit card",
-          completed: false,
-          creationDate: "2019-11-21",
-          user: 2, 
-      },
-      {
-          id : 006,
-          taskDescription: "change energy suppliers",
-          completed: true,
-          creationDate: "2019-10-30",
-          user: 1,
-      }
-    
-  ]
-});
+      {id : 001, taskDescription: "order inhalers", completed: false, creationDate: "2019-11-19", user: 2},
+      {id : 002, taskDescription: "complete shopping for trip", completed: true, creationDate: "2019-11-15", user: 1},
+      {id : 003, taskDescription: "book flight", completed: false, creationDate: "2019-11-18", user: 1},
+      {id : 004, taskDescription: "complete CV", completed: true, creationDate: 2019-11-05, user: 1}
+      {id : 005, taskDescription: "pay credit card", completed: false, creationDate: "2019-11-21", user: 2},
+      {id : 006, taskDescription: "change energy suppliers", completed: true, creationDate: "2019-10-30", user: 1} 
+    ]
+  });
 });
 
-app.delete("/tasks/:taskId", function(request, response){
-const id = request.params.taskId;
-response.status(200).send("Received a request to delete task ID: "+id);
+app.post("/tasks", function (request, response) {
+  // Do the logic for saving the new task in the DB
+  const task = request.body;
+  // { text: "do the dishes", completed: true, date: "2019" }
+  response.status(201).send("Successfully created task: " + task.text);
 });
 
-app.post("/tasks", function(request, response){
-//Create a new task
-const task = request.body;
-response.status(201).send("Created a new task with text: "+task.text);
+// my fields from state in app.js
+// text
+// completed
+// dateDone
+// dateDue
+// id
+// possibly userid
+
+app.delete("/tasks/:taskId", function (request, response) {
+  const taskId = request.params.taskId;
+  response.status(200).send("Received request to Delete task with id " + taskId);
+});
+app.put("/tasks/:taskId", function (request, response) {
+//update task
+  const taskId = request.params.taskId;
+  const updatedTask = request.body;
+  // eslint-disable-next-line no-undef
+  response.status(200).send(" Update task with id " + taskId + " with " + task.text + task.completed );
 });
 
-app.put("/tasks/:taskId",function (request, response){
-// Update task
-const id = request.params.taskId;
-const task = request.body;
-response.status(200).send("Received request to update task: " + task.text);
-});
-module.exports.tasks = serverless(app);
+
+module.exports.tasks=serverlessHttp(app);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
